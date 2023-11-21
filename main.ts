0e9d2b9f567a9ec43d3f2c906627839e9b127d31
Lineke12 <151532516+Lineke12@users.noreply.github.com>
2023-11-21 13:03:09 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,34 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        # # # # #
+        . . . . .
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # . # . #
+        # . # . #
+        # . # . #
+        # . # . #
+        # . # . #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # . # .
+        `)
+})
